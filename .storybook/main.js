bffac1e6447e5bc36bd5cc62de96703b6c57b51a
mhmdiamd <darmawanilham34@gmail.com>
2023-04-10 22:45:42 +0700

    build: implement storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+const config = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
